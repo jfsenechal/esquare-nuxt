f99dedb017aa721b570e20214b30f2f425150a83
jfsenechal <jfsenechal@gmail.com>
2024-06-06 10:44:40 +0200

    add

---
+++ components/homepage/Slogan.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+<template>
+  <section
+      class="flex flex-col items-center pr2ose lg:pros2e-xl bg-gradient-to-r from-amber-200 to-yellow-400 w-full p-10">
+    <div class="prose lg:prose-xl">
+      <h2 class="text-3xl font-semibold text-green-light">
+        " SE REUNIR EST UN DEBUT, RESTER ENSEMBLE EST UN PROGRES, TRAVAILLER ENSEMBLE EST LA CLÉ DE LA REUSSITE
+        "
+      </h2>
+      <p>Cette citation de Henry Ford est la base fondatrice d’un espace de coworking ou de co-création.</p>
+      <p>
+        C’est par la rencontre et l’échange de points de vue que les idées fleurissent ;
+        et c’est par l’échange de capacités et de compétences qu’elles se réalisent.</p>
+      <p>Si vous avez envie d’entreprendre, de créer ou de co-créer, vous êtes au bon endroit.</p>
+    </div>
+  </section>
+</template>
