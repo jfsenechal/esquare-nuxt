6073c56feddb7a6ea645aca4a72a98c191bcfe1c
jfsenechal <jfsenechal@gmail.com>
2024-07-03 15:46:36 +0200

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true},
-    modules: ["@nuxtjs/tailwindcss", ["@nuxtjs/google-fonts", {
-        families: {
-            Roboto: true
-        }
-    }
-    ]],
-    app: {
-        rootId: 'app',
-        pageTransition: {name: 'page', mode: 'out-in'}
-    },
-    runtimeConfig: {
-        public: {
-            API_URL: process.env.API_URL,
-        },
-    },
+  devtools: {enabled: true},
+
+  modules: ["@nuxtjs/tailwindcss", ["@nuxtjs/google-fonts", {
+      families: {
+          Roboto: true
+      }
+  }
+  ]],
+
+  app: {
+      rootId: 'app',
+      pageTransition: {name: 'page', mode: 'out-in'}
+  },
+
+  runtimeConfig: {
+      public: {
+          API_URL: process.env.API_URL,
+      },
+  },
+
+  compatibilityDate: '2024-07-03',
 })
