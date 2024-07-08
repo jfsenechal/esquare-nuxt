a27ce2a9aa6dfc68f30fd667dd2c49cc6ff95382
jfsenechal <jfsenechal@gmail.com>
2024-07-08 16:56:32 +0200

    fix url images

--- composables/menuItemsGet.ts
+++ composables/menuItemsGet.ts
@@ -11,7 +11,7 @@ export const news = [
     {
         name: 'Edite votre livre pour pas un balle (ou presque)',
         link: '#',
-        image: '/public/images/news/book.jpg',
+        image: '/images/news/book.jpg',
         tag: 'esquare',
         excerpt: 'Presque parce que cette formation a un coût (que vous aurez vite amorti avec quelques livres vendus).',
         color: '',
@@ -20,7 +20,7 @@ export const news = [
     {
         name: 'Forum Mind &  Market',
         link: '#',
-        image: '/public/images/news/market.jpg',
+        image: '/images/news/market.jpg',
         tag: 'esquare',
         excerpt: 'A la rencontre des entrepreneurs locaux porteurs d\'idées novatrices, qui méritent d’être mises en lumière et soutenues par la communauté Mind & Market.',
         color: '',
@@ -29,7 +29,7 @@ export const news = [
     {
         name: 'Ateliers RécréaTIC',
         link: '#',
-        image: '/public/images/news/recreatic.jpg',
+        image: '/images/news/recreatic.jpg',
         tag: 'epn',
         excerpt: 'Voici le programme des prochains ateliers numériques RécréaTIC.',
         color: '',
@@ -38,7 +38,7 @@ export const news = [
     {
         name: 'Formaction-Commerce',
         link: '#',
-        image: '/public/images/news/adl.jpg',
+        image: '/images/news/adl.jpg',
         tag: 'adl',
         excerpt: 'Participez gratuitement à ce programme d’accompagnement et mettez en œuvre rapidement les leviers de votre croissance commerciale !',
         color: '',
@@ -47,7 +47,7 @@ export const news = [
     {
         name: 'Découverte et installation de Linux',
         link: '#',
-        image: '/public/images/news/linux.jpg',
+        image: '/images/news/linux.jpg',
         tag: 'epn',
         excerpt: 'Atelier gratuit le jeudi 06 juin de 9h30 à 12h00: découverte et installation de Linux sur votre ancien ordinateur!',
         color: '',
@@ -56,7 +56,7 @@ export const news = [
     {
         name: 'Inauguration de l\E-square',
         link: '#',
-        image: '/public/images/news/inauguration.jpg',
+        image: '/images/news/inauguration.jpg',
         tag: 'esquare',
         excerpt: 'L\'e-Square, c’est le pôle dédié à la créativité des Marchoises et des Marchois et au numérique de la ville. N\'hésitez pas à franchir les portes, avec vos idées, vos projets... !',
         color: '',
@@ -65,7 +65,7 @@ export const news = [
     {
         name: 'e-Procurement',
         link: '#',
-        image: '/public/images/news/Eprocurement.jpeg',
+        image: '/images/news/Eprocurement.jpeg',
         tag: 'adl',
         excerpt: 'Une formation gratuite consacrée à la plateforme de marchés publics e-Procuremen',
         color: '',
