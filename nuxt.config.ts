38759f591be72dddc0f404841717eb6da0094663
jfsenechal <jfsenechal@gmail.com>
2024-07-08 15:33:25 +0200

    add port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
-
     modules: ["@nuxtjs/tailwindcss", ["@nuxtjs/google-fonts", {
         families: {
             Roboto: true,
             Montserrat: true,
         }
-    }
-    ]],
-
+    }]
+    ],
+    devServer: {
+        port: 3001
+    },
     app: {
         rootId: 'app',
-        port: 3001,
         pageTransition: {name: 'page', mode: 'out-in'}
     },
-
     runtimeConfig: {
         public: {
             API_URL: process.env.API_URL,
             NUXT_APP_BASE_URL: process.env.NUXT_APP_BASE_URL || '/'
         },
     },
-
     compatibilityDate: '2024-07-03',
 })
