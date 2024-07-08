f0f37a724961c9c05eca77ead03000a6684894a4
jfsenechal <jfsenechal@gmail.com>
2024-07-08 16:25:08 +0200

    remove devserver

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,17 +8,16 @@ export default defineNuxtConfig({
         }
     }]
     ],
-    devServer: {
-        port: 3001
-    },
     app: {
         rootId: 'app',
+        baseURL: '/new/',
         pageTransition: {name: 'page', mode: 'out-in'}
     },
     runtimeConfig: {
         public: {
             API_URL: process.env.API_URL,
-            NUXT_APP_BASE_URL: process.env.NUXT_APP_BASE_URL || '/'
+            NUXT_APP_BASE_URL: process.env.NUXT_APP_BASE_URL || '/',
+            NUXT_APP_URL: process.env.NUXT_APP_URL,
         },
     },
     compatibilityDate: '2024-07-03',
