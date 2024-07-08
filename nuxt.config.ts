ae72429ffb44e5a2683d60d03b0f564bdaa6e5ab
jfsenechal <jfsenechal@gmail.com>
2024-07-08 15:27:25 +0200

    add port,NUXT_APP_BASE_URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {enabled: true},
+    devtools: {enabled: true},
 
-  modules: ["@nuxtjs/tailwindcss", ["@nuxtjs/google-fonts", {
-      families: {
-          Roboto: true,
-          Montserrat: true,
-      }
-  }
-  ]],
+    modules: ["@nuxtjs/tailwindcss", ["@nuxtjs/google-fonts", {
+        families: {
+            Roboto: true,
+            Montserrat: true,
+        }
+    }
+    ]],
 
-  app: {
-      rootId: 'app',
-      pageTransition: {name: 'page', mode: 'out-in'}
-  },
+    app: {
+        rootId: 'app',
+        port: 3001,
+        pageTransition: {name: 'page', mode: 'out-in'}
+    },
 
-  runtimeConfig: {
-      public: {
-          API_URL: process.env.API_URL,
-      },
-  },
+    runtimeConfig: {
+        public: {
+            API_URL: process.env.API_URL,
+            NUXT_APP_BASE_URL: process.env.NUXT_APP_BASE_URL || '/'
+        },
+    },
 
-  compatibilityDate: '2024-07-03',
+    compatibilityDate: '2024-07-03',
 })
