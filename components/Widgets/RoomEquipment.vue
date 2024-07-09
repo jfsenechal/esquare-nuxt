7f9bf0dae6c96130f1e869c94df65a9fc5b6a7cf
jfsenechal <jfsenechal@gmail.com>
2024-07-09 17:25:02 +0200

    equipments rooms

---
+++ components/Widgets/RoomEquipment.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {IconWifi, IconCoffee, IconDeviceTv, IconDeviceProjector, IconChalkboard} from '@tabler/icons-vue'
+
+const equipments = [
+  {
+    name: 'Wifi',
+    icon: IconWifi,
+    id: 1
+  },
+  {
+    name: 'Café/thé dans la cuisine',
+    icon: IconCoffee,
+    id: 2
+  },
+  {
+    name: 'Smart TV',
+    icon: IconDeviceTv,
+    id: 3
+  },
+  {
+    name: 'Projecteur',
+    icon: IconDeviceProjector,
+    id: 4
+  },
+  {
+    name: 'Tableau blanc',
+    icon: IconChalkboard,
+    id: 5
+  }
+]
+</script>
+<template>
+  <ul class="flex flex-row gap-2 items-center">
+    <li class="pointer-events-auto flex flex-col gap-2 items-center justify-center shadow-lg p-3 w-48 h-48 hover:text-esquare-green-dark"
+        v-for="equipment in equipments" :key="equipment.id">
+      <component :is="equipment.icon"></component>
+      <span class="text-esquare-blue">{{ equipment.name }}</span>
+    </li>
+  </ul>
+</template>
--- pages/salles-a-louer/index.vue
+++ pages/salles-a-louer/index.vue
@@ -29,20 +29,24 @@ res.json().then((data) => {
           <HeaderSocialIconsSmall/>
         </div>
       </div>
-      <div v-for="room in state.rooms" :key="room.id">
-        <div class="flex flex-row space-x-40">
-          <img class="w-[30rem] h-[30rem] scale-50 animate-fade-up  animate-duration-700 opacity-0 intersect:scale-100 intersect:opacity-100 transition duration-700"
-               :src="room.Image.files[0].name" alt="room"/>
-          <div class="flex flex-col gap-5 prose lg:prose-xl">
-            <h3 class="text-2xl text-esquare-yellow font-semibold ">
-              {{ room.Nom.title[0].text.content }}
-            </h3>
-            <p class="text-base text-esquare-grey-dark prose">{{ room.Description.rich_text[0].text.content }}</p>
-            <NuxtLink :to="`salles-a-louer/reserver-zeze}`"
-                      class="animate-up text-esquare-black flex flex-row  justify-center items-center h-16 w-80 border-8 border-t-esquare-blue border-r-esquare-yellow border-b-esquare-blue border-l-esquare-yellow">
-              Réservation et tarifs
-            </NuxtLink>
-          </div>
+      <p class="text-2xl text-esquare-brown font-semibold ">
+        Toutes nos salles sont équipé de :
+      </p>
+      <WidgetsRoomEquipment/>
+      <div v-for="room in state.rooms" :key="room.id"
+           class="flex flex-row even:flex-row-reverse space-x-40 shadow-lg mb-2">
+        <img
+            class="w-full basis-1/2 h-[30rem] scale-50 animate-fade-up  animate-duration-700 opacity-0 intersect:scale-100 intersect:opacity-100 transition duration-700"
+            :src="room.Image.files[0].name" alt="room"/>
+        <div class="flex flex-col basis-1/2	gap-5 prose lg:prose-xl">
+          <h3 class="text-2xl text-esquare-yellow font-semibold ">
+            {{ room.Nom.title[0].text.content }}
+          </h3>
+          <p class="text-base text-esquare-grey-dark prose">{{ room.Description.rich_text[0].text.content }}</p>
+          <NuxtLink :to="`salles-a-louer/reserver-zeze}`"
+                    class="animate-up text-esquare-black flex flex-row  justify-center items-center h-16 w-80 border-8 border-t-esquare-blue border-r-esquare-yellow border-b-esquare-blue border-l-esquare-yellow">
+            Réservation et tarifs
+          </NuxtLink>
         </div>
       </div>
     </section>
