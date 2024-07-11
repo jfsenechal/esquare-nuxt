29f65524434d5ba426d24284d279f38a5a78e031
jfsenechal <jfsenechal@gmail.com>
2024-07-11 15:19:46 +0200

    routes spaces

--- pages/article-[slug].vue
+++ pages/article-[slug].vue
@@ -21,7 +21,7 @@ const post = {
 <template>
   <article class="flex flex-col relative">
     <ArticleHeader :icon="post.icon"
-                     :bgimage="post.image"/>
+                   :bgimage="post.image"/>
     <section class="container flex flex-col gap-2 mx-auto ms-2 md:ms-24 mt-14 min-h-80">
       <WidgetsBreadcrumb :breadcrumbs="path"/>
       <ArticleTitle>{{ post.title }}</ArticleTitle>
