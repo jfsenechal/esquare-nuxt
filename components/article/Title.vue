eef86371cd0675a25d318b4e65a1a0b14446b09f
jfsenechal <jfsenechal@gmail.com>
2024-07-11 13:54:29 +0200

    text-2xl

--- components/article/Title.vue
+++ components/article/Title.vue
@@ -2,7 +2,7 @@
 //https://codepen.io/shadeed/pen/yLpWBmP
 </script>
 <template>
-  <h1 class="w-full max-w-full flex items-center font-bold leading-5 text-4xl text-esquare-black break-words whitespace-pre-wrap pt-1 pr-1 pl-1 m-0">
+  <h1 class="w-full max-w-full flex items-center font-bold leading-5 text-2xl md:text-4xl text-esquare-black break-words whitespace-pre-wrap pt-1 pr-1 pl-1 m-0">
     <slot></slot>
   </h1>
 </template>
