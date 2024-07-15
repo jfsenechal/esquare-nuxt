b42e1e5ebc643e5070081448e5dff1cd0df1aa1e
jfsenechal <jfsenechal@gmail.com>
2024-07-15 14:24:30 +0200

    contact and remove maj widget folder

---
+++ components/widgets/Phones.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+<template>
+  <section>
+    <div class="mx-auto max-w-2xl lg:mx-0">
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900">Nos espaces</h2>
+    </div>
+    <div
+        class="mx-auto mt-16 grid  grid-cols-1 gap-8 text-base leading-7 sm:grid-cols-2 sm:gap-y-16 lg:mx-0 lg:grid-cols-4">
+      <div>
+        <h3 class="border-l border-indigo-600 pl-6 font-semibold text-gray-900">Contact général</h3>
+        <address class="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
+          <p>+32491 963 365</p>
+        </address>
+      </div>
+      <div>
+        <h3 class="border-l border-indigo-600 pl-6 font-semibold text-gray-900">Espace publique numérique</h3>
+        <address class="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
+          <p>+32 84 327 054</p>
+        </address>
+      </div>
+      <div>
+        <h3 class="border-l border-indigo-600 pl-6 font-semibold text-gray-900">FabLab</h3>
+        <address class="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
+          <p>+32490 650 266</p>
+        </address>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/contact.vue
+++ pages/contact.vue
@@ -6,23 +6,23 @@ const path = [
 ]
 </script>
 <template>
-  <article class="flex flex-col relative h-[100vh]">
+  <article class="flex flex-col relative overflow-clip">
     <ArticleHeader icon="https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/1f4ec.svg"
                    bgimage="https://images.unsplash.com/photo-1596524430615-b46475ddff6e?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=6000"/>
-    <section class="container flex flex-col gap-2 mx-auto ms-24 mt-14 min-h-80">
+    <section class="container flex flex-col gap-2 mx-auto ms-2 md:ms-24 mt-14 min-h-80">
       <WidgetsBreadcrumb :path/>
       <ArticleTitle>Contactez-nous</ArticleTitle>
-      <div class="flex flex-row justify-end">
-        <div class="flex flex-col items-center gap-2">
-          <h4 class="text-esquare-blue">Partager sur</h4>
-          <HeaderSocialIconsSmall/>
+      <WidgetsShareBox/>
+        <div class="mt-6 min-h-svh p-4 prose lg:prose-xl">
+          <p class="mt-6 text-lg prose lg:prose-xl text-gray-600">
+            N'hésitez pas à nous contacter : l’e-Square est accessible à tout le monde, particulier ou entreprise,
+            indépendant ou salarié, maker, bricoleur...<br>
+            Une personne de contact est habituellement présente de 9h à 17h en semaine (jusque 15h le vendredi)... mais
+            le mieux est de prévenir de votre venue par e-mail ou par téléphone !
+          </p>
+          <WidgetsPhones/>
+          <WidgetsContactForm/>
         </div>
-      </div>
-      <div class="mt-6 min-h-svh p-4 prose lg:prose-xl">
-        Formulaire contact
-
-        <h4 class="text-xl p-8">Plans, téléphones par secteur, ...</h4>
-      </div>
     </section>
   </article>
 </template>
