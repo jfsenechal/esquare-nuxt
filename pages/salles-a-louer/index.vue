364a393bc03b7fe090c0bc4ef9230cf699c0ab3c
jfsenechal <jfsenechal@gmail.com>
2024-07-16 14:18:09 +0200

    page reserver

--- pages/salles-a-louer/index.vue
+++ pages/salles-a-louer/index.vue
@@ -36,16 +36,16 @@ res.json().then((data) => {
         </p>
         <WidgetsRoomEquipment/>
         <div v-for="room in rooms" :key="room.id"
-             class="flex flex-row items-center even:flex-row-reverse mb-2 -translate-x-1/3 intersect:translate-x-0 transition ease-out duration-500">
+             class="flex flex-col md:flex-row items-center md:even:flex-row-reverse mb-2 -translate-x-1/3 intersect:translate-x-0 transition ease-out duration-500">
           <img
-              class="w-[50rem]"
+              class="w-[18rem] md:w-[50rem] mb-2 md:mb-0"
               :src="room.Image.files[0].name" alt="room"/>
           <div class="flex flex-col items-center prose lg:prose-xl ml-4 ">
             <h3 class="text-2xl text-esquare-yellow font-semibold ">
               {{ room.Nom.title[0].text.content }}
             </h3>
             <span class="text-esquare-grey-dark">{{ room.Description.rich_text[0].text.content }}</span>
-            <NuxtLink :to="`salles-a-louer/reserver-zeze}`"
+            <NuxtLink :to="`salles-a-louer/reserver-zeze`"
                       class="animate-up text-esquare-black flex flex-row  justify-center items-center h-16 w-80 border-8 border-t-esquare-blue border-r-esquare-yellow border-b-esquare-blue border-l-esquare-yellow">
               Réservation et tarifs
             </NuxtLink>
--- pages/salles-a-louer/reserver-[slug].vue
+++ pages/salles-a-louer/reserver-[slug].vue
@@ -6,6 +6,15 @@ const post = {title: 'Tournoi Super Smash Bros', link: '/', icon: ''}
 </script>
 <template>
   <article class="flex flex-col relative">
-
+    <ArticleHeader icon="https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/1f4c5.svg"
+                     bgimage="https://images.unsplash.com/photo-1617106400337-66e7d72a466e?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=4800"/>
+    <section class="container flex flex-col gap-2 mx-auto ms-2 md:ms-24 mt-14 min-h-80">
+      <WidgetsBreadcrumb :path/>
+      <ArticleTitle>Réserver la salle X</ArticleTitle>
+      <WidgetsShareBox/>
+      <div class="mt-6 min-h-svh p-4 prose lg:prose-xl">
+        <h3 class="text-2xl">Calendrier + formulaire</h3>
+      </div>
+    </section>
   </article>
 </template>
