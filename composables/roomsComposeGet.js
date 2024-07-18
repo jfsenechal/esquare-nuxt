13f910884fa1c07589321e2cbc7ae40c462ad88b
jfsenechal <jfsenechal@gmail.com>
2024-07-18 11:04:03 +0200

    add api url grr

--- composables/roomsComposeGet.js
+++ composables/roomsComposeGet.js
@@ -2,7 +2,7 @@ export default () => {
     const config = useRuntimeConfig()
     const {pending: pending, data: data, error: error} =
         useFetch(
-            `${config.public.API_URL}/api/rooms`, {}
+            `${config.public.API_URL_SERVER}/api/rooms`, {}
         )
     return {
         pending,
