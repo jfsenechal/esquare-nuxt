f22f877b4eed304bf66954c71c47e0b3ca67e6cf
jfsenechal <jfsenechal@gmail.com>
2024-07-23 14:15:14 +0200

    fix css mobile

--- components/room/Inline.vue
+++ components/room/Inline.vue
@@ -10,18 +10,26 @@ const slugify = computed(() => {
     .replace(/\s+/g, '')  // Remove spaces instead of replacing with hyphens
     .replace(/[^\w]+/g, '')  // Remove any non-word characters
 })
+const roomName = computed(() => {
+  return room.value.Nom.title[0].text.content
+})
+const roomDescription = computed(() => {
+  return room.value.Description.rich_text[0].text.content
+})
 </script>
 <template>
   <div
       class="flex flex-col md:flex-row items-center md:even:flex-row-reverse mb-2 even:translate-x-1/3 -translate-x-1/3 intersect:translate-x-0 transition ease-out duration-500">
     <img
-        class="w-[18rem] md:w-[50rem] mb-2 md:mb-0"
+        class="w-full md:w-[50rem] mb-2 md:mb-0"
         :src="room.Image.files[0].name" alt="room"/>
-    <div class="flex flex-col items-center prose lg:prose-xl ml-4 ">
-      <h3 class="text-2xl text-esquare-yellow font-semibold ">
-        {{ room.Nom.title[0].text.content }}
+    <div class="flex flex-col items-center">
+      <h3 class="text-2xl text-esquare-yellow font-semibold">
+        {{ roomName }}
       </h3>
-      <span class="text-esquare-grey-dark text-left trail">{{ room.Description.rich_text[0].text.content }}</span>
+      <span class="text-esquare-grey-dark text-left prose lg:prose-xl ">
+        {{ roomDescription }}
+      </span>
       <RoomFeatures :room :key="room.GrrId.number"/>
       <NuxtLink :to="`salles-a-louer/reserver-${slugify}-${room.GrrId.number}`"
                 :key="room.GrrId.number"
