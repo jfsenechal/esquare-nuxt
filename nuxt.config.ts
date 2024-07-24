eb6064d427db0791933439c1dccbeb847080df26
jfsenechal <jfsenechal@gmail.com>
2024-07-24 16:36:43 +0200

    remove prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
     },
     nitro: {
         prerender: {
-            routes: ['/salles-a-louer'],
+            routes: [],
         },
     },
     compatibilityDate: '2024-07-03',
