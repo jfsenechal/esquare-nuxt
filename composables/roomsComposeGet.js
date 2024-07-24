db1e76986a51072c20ca0a5cb38b7ce9f5db0cc8
jfsenechal <jfsenechal@gmail.com>
2024-07-24 11:36:30 +0200

    clean

--- composables/roomsComposeGet.js
+++ composables/roomsComposeGet.js
@@ -1,9 +1,7 @@
 export default (roomId = 0) => {
     const config = useRuntimeConfig()
     const {pending: pending, data: data, error: error} =
-        useFetch(
-            `${config.public.API_URL_SERVER}/api/rooms`, {}
-        )
+        useFetch(`${config.public.API_URL_SERVER}/api/rooms`, {})
     return {
         pending,
         data,
