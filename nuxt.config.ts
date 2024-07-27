bd6c49c345c9d047ce33bbc359ef74fc63d6f7ac
jfsenechal <jfsenechal@gmail.com>
2024-07-27 19:25:31 +0200

    add space id

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
         public: {
             API_URL_SERVER: process.env.API_URL_SERVER,
             API_URL_GRR: process.env.API_URL_GRR,
+            NOTION_ESPACE_ID: process.env.NOTION_ESPACE_ID,
             NUXT_APP_BASE_URL: process.env.NUXT_APP_BASE_URL || '/',
         },
     },
