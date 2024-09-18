34b24155e1adffb7672519df925d49e592516af9
jfsenechal <jfsenechal@gmail.com>
2024-09-18 16:27:21 +0200

    begin new menu

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,21 +1,90 @@
 <script setup>
 const mobileMenu = ref(false)
+import items from '~/composables/navigationsGet.js';
 </script>
 <template>
-  <header class="w-full grid grid-cols-[auto_1fr] grid-rows-2 md:grid-rows-1 gap-0 h-24 lg:h-32 items-center">
-    <button type="button" aria-label="Menu mobile" @click.prevent="mobileMenu = !mobileMenu" class="mt-2 md:hidden">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-           class="w-12 h-12">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-      </svg>
-    </button>
-    <NuxtLink to="/" class="ml-4 mt-4">
-      <NuxtPicture src="/images/EsquareLogoBlackWhite.png" alt="logo" id="logoEsquare"
-                   :imgAttrs="{class:'transition-all duration-500\tease-in-out\tanimate-bouncejf h-12 md:h-16'}"/>
-    </NuxtLink>
-    <div class="col-span-2 md:col-span-1 justify-self-end mr-4 flex flex-col items-end justify-around space-y-4">
-      <HeaderTopNavigation :mobileMenu="mobileMenu"/>
-      <HeaderSocialIconsLight svg-fill/>
+  <header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:divide-gray-200 lg:px-8">
+      <div class="relative flex h-16 justify-between">
+        <div class="relative z-10 flex px-2 lg:px-0">
+
+          <div class="flex flex-shrink-0 items-center">
+            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                 alt="Your Company">
+          </div>
+        </div>
+        <div class="relative z-0 flex flex-1 items-center justify-center px-2 sm:absolute sm:inset-0">
+          <div class="w-full sm:max-w-xs">
+            <div class="relative">
+              <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd"
+                        d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                        clip-rule="evenodd"/>
+                </svg>
+              </div>
+              <a id="search"
+                 href="/contact"
+                 class="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                Contactez-nous
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="relative z-10 flex items-center">
+          <!-- Mobile menu button -->
+          <button type="button" @click.prevent="mobileMenu = !mobileMenu"
+                  class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                  aria-controls="mobile-menu" aria-expanded="false">
+            <span class="absolute -inset-0.5"></span>
+            <span class="sr-only">Open menu</span>
+            <!--
+              Icon when menu is closed.
+
+              Menu open: "hidden", Menu closed: "block"
+            -->
+            <svg class="h-6 w-6"
+                 :class="mobileMenu ? 'hidden':'block'"
+                 fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                 aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+            </svg>
+            <!--
+              Icon when menu is open.
+
+              Menu open: "block", Menu closed: "hidden"
+            -->
+            <svg class="h-6 w-6"
+                 :class="mobileMenu ? 'block':'hidden'"
+                 fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                 aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
+    <!-- Mobile menu, show/hide based on menu state. -->
+    <nav
+        class="overflow-hidden text-center z-20 transition-all duration-700 ease-in-out"
+        :class="mobileMenu ? 'h-screen' : 'h-0'"
+        aria-label="Global" id="mobile-menu">
+      <div class="space-y-1 px-2 pb-3 pt-2">
+        <!-- todo Current: "bg-gray-100 text-gray-900", Default: "text-gray-900 hover:bg-gray-50 hover:text-gray-900" -->
+        <NuxtLink v-for="item in items"
+                  :key="item.id"
+                  :to="item.link"
+                  @click="mobileMenu = !mobileMenu"
+                  class="animateText block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-50 hover:text-gray-900 uppercase"
+                  aria-current="page">
+          {{ item.name }}
+        </NuxtLink>
+      </div>
+    </nav>
   </header>
 </template>
+<style>
+.animateText {
+  @apply relative after:absolute after:bg-esquare-yellow after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300
+}
+</style>
