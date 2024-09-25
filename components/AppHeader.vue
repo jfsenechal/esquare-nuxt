d2b2c153dc72af8ea51efb82e6d7be66c1a3beae
jfsenechal <jfsenechal@gmail.com>
2024-09-25 12:15:31 +0200

    buuuuuuuuuug

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -8,76 +8,46 @@ const {
 const mobileMenu = ref(false)
 </script>
 <template>
-  <header class="bg-white/5 shadow sticky top-0  backdrop-blur-md z-30">
-    <div class="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:divide-gray-200 lg:px-8">
-      <div class="relative flex h-20 justify-between">
-        <div class="relative z-10 flex px-2 lg:px-0">
-          <NuxtLink to="/" class="flex flex-shrink-0 items-center">
-            <NuxtPicture src="/images/EsquareLogoBlackWhite.png" alt="logo" id="logoEsquare"
-                         :imgAttrs="{class:'transition-all duration-500 tease-in-out animate-bouncejf h-16 md:h-16 w-auto '}"/>
-          </NuxtLink>
-        </div>
-        <div class="relative z-0 flex flex-1 items-center justify-center px-2 sm:absolute sm:inset-0">
-          <div class="w-full sm:max-w-xs">
-            <div class="relative">
-              <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fill-rule="evenodd"
-                        d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                        clip-rule="evenodd"/>
-                </svg>
-              </div>
-              <NuxtLink to="/contact"
-                        id="search"
-                        class="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                Contactez-nous
-              </NuxtLink>
-            </div>
-          </div>
-        </div>
-        <div class="relative z-10 flex items-center">
-          <!-- Mobile menu button -->
-          <button type="button" @click.prevent="mobileMenu = !mobileMenu"
-                  class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                  aria-controls="mobile-menu" aria-expanded="false">
-            <span class="absolute -inset-0.5"></span>
-            <span class="sr-only">Open menu</span>
-            <!--
-              Icon when menu is closed.
+  <header
+      class="grid grid-cols-[auto_1fr] h-32 lg:h-32 grid-rows-2 md:grid-rows-1 bg-white/5 shadow sticky top-0 backdrop-blur-md z-30">
+    <div class="relative z-30 flex items-center">
+      <!-- Mobile menu button -->
+      <button type="button" @click.prevent="mobileMenu = !mobileMenu"
+              class="relative inline-flex items-center justify-center rounded-md text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-esquare-yellow"
+              aria-controls="mobile-menu" aria-expanded="false">
+        <span class="absolute -inset-0.5"></span>
+        <span class="sr-only">Open menu</span>
+        <!--
+          Icon when menu is closed.
 
-              Menu open: "hidden", Menu closed: "block"
-            -->
-            <svg class="h-12 w-12"
-                 :class="mobileMenu ? 'hidden':'block'"
-                 fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                 aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-            </svg>
-            <!--
-              Icon when menu is open.
+          Menu open: "hidden", Menu closed: "block"
+        -->
+        <svg class="h-12 w-12"
+             :class="mobileMenu ? 'hidden':'block'"
+             fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+             aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+        </svg>
+        <!--
+          Icon when menu is open.
 
-              Menu open: "block", Menu closed: "hidden"
-            -->
-            <svg class="h-12 w-12"
-                 :class="mobileMenu ? 'block':'hidden'"
-                 fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                 aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
-            </svg>
-          </button>
-        </div>
-      </div>
+          Menu open: "block", Menu closed: "hidden"
+        -->
+        <svg class="h-12 w-12"
+             :class="mobileMenu ? 'block':'hidden'"
+             fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+             aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+        </svg>
+      </button>
+    </div>
+    <NuxtLink to="/" class="flex items-center mt-2 mx-2 ">
+      <img src="/images/EsquareLogoBlackWhite.png" alt="logo" id="logoEsquare"
+           class="h-12 transition-all duration-500 tease-in-out animate-bouncejf md:h-16 w-auto"/>
+    </NuxtLink>
+    <div class="col-span-2 md:col-span-1 justify-self-end mr-4 flex flex-col items-end justify-around space-y-4">
+      <HeaderSocialIconsLight svg-fill/>
     </div>
     <!-- Mobile menu, show/hide based on menu state. -->
-    <nav
-        class="overflow-hidden text-center z-20 transition-all duration-700 ease-in-out"
-        :class="mobileMenu ? 'h-screen' : 'h-0'"
-        aria-label="Global" id="mobile-menu">
-      <div class="space-y-1 px-2 pb-3 pt-2" v-if="status==='pending'">Chargement...</div>
-      <div class="space-y-1 px-2 pb-3 pt-2" v-else-if="error">{{ error }}</div>
-      <div class="space-y-1 px-2 pb-3 pt-2" v-else>
-        <HeaderMenuItems :child-pages="data.childPages" v-model:mobile-menu="mobileMenu"/>
-      </div>
-    </nav>
   </header>
 </template>
